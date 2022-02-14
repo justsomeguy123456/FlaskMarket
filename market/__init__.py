8aaa29a9544a73f95fc82a49dd6d3181f28ec766
Nick <nickabbasi1@gmail.com>
2022-02-14 18:34:31 -0500

    added login script

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask , render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 
 app.config['SECRET_KEY'] = '61273bcdec798caa73fbb1a8'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,9 +1,9 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item,User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
-
+from flask_login import login_user
 @app.route("/")
 @app.route('/home')
 def home_page():
@@ -21,7 +21,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password_hash=form.password1.data,
+                              password=form.password1.data,
                                 )
         db.session.add(user_to_create)
         db.session.commit()
@@ -30,3 +30,16 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f' error = {err_msg}', category='danger')
     return render_template('register.html', form = form)
+
+@app.route('/login', methods=['GET','Post'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(attempted_password = form.password.data):
+            login_user(attempted_user)
+            flash(f'Logged in as {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('User name and password wrong', category = 'danger')
+    return render_template('login.html' , form = form)
